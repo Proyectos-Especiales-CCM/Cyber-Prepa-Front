dd0bc94666925a6af5c7acfd26851f1be824d85c
feraranas <46a12sFAASfaas>
2024-01-08 10:12:07 -0600

    services index

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,13 +1,21 @@
 // ------------------------------------------------------------------- GET
-export {  } from "";
+export { readUserMe } from "./users/readUser(me)";
+export { readUserById } from "./users/readUserById";
+export { readUsers } from "./users/readUsers";
 
 
 // ------------------------------------------------------------------- POST
-export { createUser } from "./users/addUserToGame";
+export { createUser } from "./users/createUser";
+export { refreshToken } from "./token/refreshToken";
+export { obtainToken } from "./token/obtainToken";
 
 
 // ------------------------------------------------------------------- PUT
-export {  } from "";
+export { updateUserById } from "./users/updateUserById";
+
+
+// ------------------------------------------------------------------- PATCH
+export { patchUserById } from "./users/patchUserById";
 
 
 // ------------------------------------------------------------------- DELETE
