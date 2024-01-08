019b2b1e90d0c652ebc6837ed642284e661dd019
feraranas <46a12sFAASfaas>
2024-01-08 10:10:01 -0600

    update user by id

---
+++ src/services/users/updateUserById.tsx
@@ -0,0 +1,34 @@
+// Read Users
+import httpInstance from "../httpInstance";
+
+export const updateUserById = async (
+          userId: number,
+          email: string,
+          password: string,
+          is_admin: boolean,
+          theme: string,
+          is_active: boolean
+     ) => {
+    let res;
+    const endpoint = `users/?id=${userId}`;
+
+    const requestBody = {
+          email: email,
+          password: password,
+          is_admin: is_admin,
+          theme: theme,
+          is_active: is_active  
+     }
+
+    await httpInstance
+        .put(endpoint, JSON.stringify(requestBody), {
+            headers: { 'Content-Type': 'application/json' },
+        })
+        .then((response) => {
+            res = response;
+        })
+        .catch((error) => {
+            res = error.response;
+        });
+    return res;
+};
