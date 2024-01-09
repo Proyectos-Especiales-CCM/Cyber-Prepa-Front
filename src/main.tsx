e7847f6c04134134649c9e6984b0e27ee23e376b
feraranas <46a12sFAASfaas>
2024-01-08 23:36:12 -0600

    context provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { AppContextProvider } from './store/appContext/appContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <AppContextProvider>
     <App />
-  </React.StrictMode>,
+  </AppContextProvider>
 )
