59ef662aba1f58d3568ae2b0090d33de7b084268
feraranas <46a12sFAASfaas>
2024-01-09 00:38:18 -0600

    route constants paths

--- src/routes/Constants.ts
+++ src/routes/Constants.ts
@@ -1,13 +1,7 @@
 export const ROUTES = {
-     // PUBLIC
      HOME: "/",
-     HOME_ADMIN: "/login",
-
-     // ADMIN
-     ADMIN_PROFILE: "/admin",
-   
-     // USER
-     HOME_USER: "/user",
-     PROFILE: "/profile",
-};
-   
+     LOGIN: "/login",
+     REGULATIONS: "/regulations",
+     ADMIN: "/admin",
+     REPORTS: "/reports",
+}
