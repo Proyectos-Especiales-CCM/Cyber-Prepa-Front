9a8034935bdb45b5ff65ffbb977d4b8392538cd1
feraranas <46a12sFAASfaas>
2024-01-09 00:40:04 -0600

    app context utils

---
+++ src/store/appContext/types.ts
@@ -0,0 +1,28 @@
+import React from "react";
+
+export type UserObject = {
+  id: string;
+  email: string;
+  isAdmin: boolean;
+  theme: string;
+  isActive: boolean;
+};
+
+export interface Tokens {
+  access_token: string;
+  refresh_token: string;
+}
+
+export type AppState = {
+  user: UserObject | undefined;
+  setUser: (user: UserObject) => void;
+  tokens: Tokens | undefined;
+  setTokens: (accessToken: string, refreshToken: string) => void;
+  admin: boolean;
+  setIsAdmin: (isAdmin: boolean) => void;
+  logOut: () => void;
+};
+
+export type AppContextProps = {
+  children: React.ReactNode;
+};
