dab5ab1b0aac6a6ea31725394a4092d7c595854f
feraranas <46a12sFAASfaas>
2024-01-17 13:42:26 -0600

    init countdowns for each card function

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,7 @@
 import { initCardsFunctionality } from './initCardsFunctionality';
 import { UserObject } from '../../store/appContext/types';
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { initCountdown } from './initCountdown';
 import { game } from '../../pages/Home/types'
 import { CardExpander } from '..';
 import './Card.css';
@@ -13,13 +14,18 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ cardGame, user }) => {
 
      const cardsRef = useRef<HTMLDivElement>(null);
-
+     const countdownRef = useRef<HTMLDivElement>(null);
+     
      useEffect(() => {
           if (user !== undefined) {
                initCardsFunctionality(cardsRef)
           }
      }, [user]);
 
+     useEffect(() => {
+          initCountdown(cardGame, countdownRef);
+     }, [])
+     
      return (
      
           <div key={cardGame.id} className="cyber__card [ is-collapsed ]">
@@ -31,13 +37,16 @@ const Card: React.FC<CardProps> = ({ cardGame, user }) => {
                     <div id={`cyber__game__players__count__${cardGame.id}`}>
 
                          <span>
-                              {Array.isArray(cardGame.plays) ? `${cardGame.plays.length} jugadores` : `${cardGame.plays} jugadores`}
+                              {Array.isArray(cardGame.plays) ? 
+                                   (cardGame.plays.length == 1 ? `1 jugador` : `${cardGame.plays.length} jugadorxs`)  :
+                                   (cardGame.plays == 1 ? `1 jugador` : `${cardGame.plays} jugadorxs`)
+                              }
                          </span><br />
                          
                          <img className="cyber__image" src={`/static/${cardGame.file_route}`} alt={`${cardGame.name}`} />
                          
                          <div className="remaining__time">
-                              <p id={`cyber__countdown__${cardGame.id}`}>No data</p>
+                              <p ref={countdownRef} id={`cyber__countdown__${cardGame.id}`}></p>
                          </div>
                     </div>
                </div>
