ccb9d195a306da4935571e21de6dcd80c219dda9
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-01-31 08:26:06 -0600

    Separar estilos

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,43 @@ const Header = () => {
 
   return (
     <header style={{ zIndex: 100 }} className="position-sticky top-0">
-      HEADER 😇
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">CyberTec</span>
+          <div className="d-flex">
+            <span className="navbar-text me-2">{user?.email}</span>
+            <div className="d-flex align-items-center">
+              <button
+                className="btn btn-outline-primary me-2"
+                onClick={() => navigate("/")}
+              >
+                Home
+              </button>
+              <button
+                className="btn btn-outline-primary me-2"
+                onClick={() => navigate("/admin")}
+              >
+                Admin
+              </button>
+              <button
+                className="btn btn-outline-primary me-2"
+                onClick={() => navigate("/login")}
+              >
+                Login
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => {
+                  logOut();
+                  navigate("/");
+                }}
+              >
+                Log Out
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };
