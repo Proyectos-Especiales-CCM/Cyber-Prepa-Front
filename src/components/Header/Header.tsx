b42f44d8074c69731fa4a3e71bd9368c82cf5a08
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-02-09 11:22:47 -0600

    Fix error message Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -196,18 +196,14 @@ const Header = () => {
                 onClose={handleCloseUserMenu}
               >
                 {user ? (
-                  <>
-                    <button
-                      onClick={() => {
-                        logOut();
-                        handleCloseUserMenu()
-                        navigate("/");
-                      }}
-                    >
-                      Log out
-                    </button>
-                  </>
-                ): (<Button onClick={() => navigate("/login")}>Login</Button>)}
+                  <MenuItem key="logout" onClick={() => {
+                    logOut();
+                    handleCloseUserMenu();
+                    navigate("/");
+                  }}>
+                    <Typography>Log out</Typography>
+                  </MenuItem>
+                ) : (<Button onClick={() => navigate("/login")}>Login</Button>)}
                     
               </Menu>
             </Box>
@@ -218,46 +214,3 @@ const Header = () => {
   );
 }
 export default Header;
-
-
-// return (
-//   <header style={{ zIndex: 100 }} className="position-sticky top-0">
-//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//       <div className="container-fluid">
-//         <span className="navbar-brand mb-0 h1">CyberTec</span>
-//         <div className="d-flex">
-//           <span className="navbar-text me-2">{user?.email}</span>
-//           <div className="d-flex align-items-center">
-//             <button
-//               className="btn btn-outline-primary me-2"
-//               onClick={() => navigate("/")}
-//             >
-//               Home
-//             </button>
-//             <button
-//               className="btn btn-outline-primary me-2"
-//               onClick={() => navigate("/admin")}
-//             >
-//               Admin
-//             </button>
-//             <button
-//               className="btn btn-outline-primary me-2"
-//               onClick={() => navigate("/login")}
-//             >
-//               Login
-//             </button>
-//             <button
-//               className="btn btn-outline-danger"
-//               onClick={() => {
-//                 logOut();
-//                 navigate("/");
-//               }}
-//             >
-//               Log Out
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </nav>
-//   </header>
-// );
