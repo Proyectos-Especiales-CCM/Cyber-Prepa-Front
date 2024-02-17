938ca6f66fa190433d3c03ed87fc4b5ebc3916d7
feraranas <46a12sFAASfaas>
2024-02-17 15:48:59 -0600

    refactor, to avoid reusing websocket communication in chidlren

--- src/components/CollapsedStudents/CollapsedStudents.tsx
+++ src/components/CollapsedStudents/CollapsedStudents.tsx
@@ -1,56 +1,13 @@
 import { CollapsedStudentItem } from "..";
-import { useEffect, useMemo, useState } from "react";
-import useWebSocket from 'react-use-websocket';
+import { useState } from "react";
 import { Game, Play } from "../../services/types";
-import { readGameById, readPlayById } from "../../services";
 
 interface CollapsedStudentProps {
   cardGame: Game;
 }
 
 const CollapsedStudents: React.FC<CollapsedStudentProps> = ({ cardGame }) => {
-  const [socketUrl] = useState('ws://172.174.255.29/ws/updates/');
-  const { lastMessage } = useWebSocket(socketUrl);
   const [playsData, setPlaysData] = useState<Play[] | number>(cardGame.plays);
-  const [forceRender, setForceRender] = useState(false);
-  const [accessToken, setAccessToken] = useState<string>('');
-
-  useEffect(() => {
-    const tokensString = localStorage.getItem('tokens');
-    if (tokensString) {
-      const tokens = JSON.parse(tokensString);
-      setAccessToken(tokens.access_token);
-    }
-  }, []);
-  
-  useEffect(() => {
-    if (lastMessage) {
-      const data = JSON.parse(lastMessage.data);
-      const message = data.message;
-
-      if (message === 'Plays updated') {
-        const gameId = data['info'];
-        if (cardGame.id === gameId) {
-          console.log("should only print once", gameId)
-
-          readGameById(gameId, accessToken)
-            .then((response) => {
-              
-              if (response.status === 200) {
-                const newPlaysData = response.data[gameId];
-                console.log(newPlaysData)
-                
-              }
-            })
-            .catch((error) => {
-              console.error('Error updating game:', error);
-            })
-          
-          setForceRender((prev) => !prev);
-        }
-      }
-    }
-  }, [lastMessage, playsData]);
 
   return (
     <div className="collapsed__students">
