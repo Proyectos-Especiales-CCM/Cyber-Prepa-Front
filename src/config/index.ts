58c8086407062f5973e635abd3a10b6a79872998
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-02-20 13:28:54 -0600

    Fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,6 @@
 const Config = {
      // It can be here, or we can change it to the .env
-     //API_URL: 'http://172.174.255.29/',
-     API_URL: 'http://127.0.0.1:8000/',
+     API_URL: 'http://172.174.255.29/',
  }
  
 export default Config;
